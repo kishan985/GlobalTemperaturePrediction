60ab4da71042d329db6159cc2801dcb1d95e5739
kishan987 <kishanpatel.1585@gmail.com>
2022-12-14 23:09:00 -0500

    Normalized Raw data to 3(Major Cities, Country and Global) datasets.

--- Project.py
+++ Project.py
@@ -53,7 +53,6 @@ with open('GlobalLandTemperaturesByMajorCity.csv', 'r') as file:
             continue
         if header is None:
             header = line.strip().split(',')[0:5]
-            print(header)
             continue
         MajorCities.append(line.strip().split(',')[0:5])
 
@@ -75,13 +74,76 @@ for i in range(len(MajorCities)):
         continue
     if MajorCities[i][1] == '' or MajorCities[i][2] == '':
         continue
-    majorcityrecords.append((MajorCities[i][0], MajorCities[i][3], MajorCities[i][4], round(float(MajorCities[i][1]), 2), MajorCities[i][2]))
+    majorcityrecords.append((MajorCities[i][0], MajorCities[i][3], MajorCities[i][4], round(float(MajorCities[i][1]), 2), round(float(MajorCities[i][2]),2)))
 
 with conn:
     cur.executemany('''INSERT INTO GlobalLandTemperatureByMajorCity(Date, City, Country, AverageTemperature, AverageTemperatureUncertainity)
                         VALUES (?,?,?,?,?)''', majorcityrecords)
 
-print('Check')
+header = None
+Countries = []
+with open('GlobalLandTemperaturesByCountry.csv', 'r') as file:
+    for line in file:
+        if not line.strip(): # used for skipping empty lines!
+            continue
+        if header is None:
+            header = line.strip().split(',')
+            continue
+        Countries.append(line.strip().split(','))
+
+query = '''CREATE TABLE [GlobalLandTemperatureByCountry](
+                [CountryID] INTEGER NOT NULL PRIMARY KEY,
+                [Date] VARCHAR NOT NULL,
+                [Country] INTEGER NOT NULL,
+                [AverageTemperature] REAL,
+                [AverageTemperatureUncertainity] REAL)
+            '''
+create_table(conn, query)
+countryrecords = []
+for i in range(len(Countries)):
+    if int(Countries[i][0][0:4]) < 1900:
+        continue
+    if Countries[i][1] == '' or Countries[i][2] == '':
+        continue
+    countryrecords.append((Countries[i][0], Countries[i][3], round(float(Countries[i][1]), 2), round(float(Countries[i][2]), 2)))
+
+with conn:
+    cur.executemany('''INSERT INTO GlobalLandTemperatureByCountry(Date, Country, AverageTemperature, AverageTemperatureUncertainity)
+                        VALUES (?,?,?,?)''', countryrecords)
+
+header = None
+GlobalTemperatures = []
+with open('GlobalTemperatures.csv', 'r') as file:
+    for line in file:
+        if not line.strip(): # used for skipping empty lines!
+            continue
+        if header is None:
+            header = line.strip().split(',')[0:3]
+            continue
+        GlobalTemperatures.append(line.strip().split(',')[0:3])
+
+query = '''CREATE TABLE [GlobalTemperatures](
+                [ID] INTEGER NOT NULL PRIMARY KEY,
+                [Date] VARCHAR NOT NULL,
+                [AverageTemperature] REAL,
+                [AverageTemperatureUncertainity] REAL)
+            '''
+create_table(conn, query)
+globalrecords = []
+for i in range(len(GlobalTemperatures)):
+    if int(GlobalTemperatures[i][0][0:4]) < 1900:
+        continue
+    if GlobalTemperatures[i][1] == '' or GlobalTemperatures[i][2] == '':
+        continue
+    globalrecords.append((GlobalTemperatures[i][0], round(float(GlobalTemperatures[i][1]), 2), round(float(GlobalTemperatures[i][2]), 2)))
+
+with conn:
+    cur.executemany('''INSERT INTO GlobalTemperatures(Date, AverageTemperature, AverageTemperatureUncertainity)
+                        VALUES (?,?,?)''', globalrecords)
+
+'''Select CAST(strftime('%Y',Date)  AS INT) AS Year, Country, AverageTemperature, 
+    AverageTemperatureUncertainity from GlobalLandTemperatureByCountry GROUP BY Year, 
+    Country ORDER BY Country;'''
 
 
 
