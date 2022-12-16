cc5d329d34d5f7563cdc84d69b6b16d6c76cc381
kishan987 <kishanpatel.1585@gmail.com>
2022-12-16 14:44:36 -0500

    Normalized the database for major cities table.

--- Project.py
+++ Project.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as pp
 import sqlite3
 from sqlite3 import Error
 from datetime import datetime
+# Load Statsmodels 
+import statsmodels.api as sm
+
 
 #GlobalVariables
 db_file_name = 'normalized_Temperature.db'
-
-# df = pd.read_csv('archive/GlobalLandTemperaturesByMajorCity.csv')
-# df = df.drop(['Latitude', 'Longitude'], axis = 1)
 
 def create_connection(db_file, delete_db=False):
     import os
@@ -46,41 +46,106 @@ def execute_sql_statement(sql_statement, conn):
     rows = cur.fetchall()
     return rows
 
-header = None
-MajorCities = []
-with open('GlobalLandTemperaturesByMajorCity.csv', 'r') as file:
-    for line in file:
-        if not line.strip(): # used for skipping empty lines!
-            continue
-        if header is None:
-            header = line.strip().split(',')[0:5]
-            continue
-        MajorCities.append(line.strip().split(',')[0:5])
-
 # print(datetime.strptime(MajorCities[0][0], '%Y-%m-%d').strftime('%Y-%m-%d'))
 
 conn = create_connection(db_file_name)
 cur = conn.cursor()
 
-query = '''CREATE TABLE [GlobalLandTemperatureByMajorCity](
+header = None
+Cities = []
+Countries = []
+MCities = []
+with open('GlobalLandTemperaturesByMajorCity.csv', 'r') as file:
+    count = 1
+    for line in file:
+        if not line.strip(): # used for skipping empty lines!
+            continue
+        if header is None:
+            header = line.strip().split(',')[0:7]
+            continue
+        temp_lst = line.strip().split(',')
+        Cities.append(temp_lst)
+        if temp_lst[4] not in Countries:
+            Countries.append(temp_lst[4])
+        if (temp_lst[3], temp_lst[5], temp_lst[6]) not in MCities:
+            MCities.append((temp_lst[3], temp_lst[5], temp_lst[6]))
+
+
+Cities.sort()
+
+#Creating Major City Table 
+sql_query = '''CREATE TABLE [MajorCities](
                 [CityID] INTEGER NOT NULL PRIMARY KEY,
+                [CityName] TEXT NOT NULL,
+                [Latitude] TEXT NOT NULL,
+                [Longitude] TEXT NOT NULL)
+            '''
+create_table(conn, sql_query)
+
+
+# Inserting into Major City Table
+with conn:
+    cur = conn.cursor()
+    cur.executemany('''INSERT INTO MajorCities(CityName, Latitude, Longitude)
+                        VALUES (?,?,?)''',MCities )
+
+#Creating Country Table 
+sql_query = '''CREATE TABLE [Country](
+                [CountryID] INTEGER NOT NULL PRIMARY KEY,
+                [Country] TEXT NOT NULL)
+            '''
+create_table(conn, sql_query)
+
+Country_list = []
+for c in Countries:
+    Country_list.append((c, ))
+
+Country_list.sort()
+
+# Inserting into Country Table
+with conn:
+    cur = conn.cursor()
+    cur.executemany('''INSERT INTO Country(Country)
+                        VALUES (?)''',Country_list )
+
+# Creating Major Cities dictionary
+sql_statement = """ SELECT CityID, CityName FROM MajorCities; """
+cityrows = execute_sql_statement(sql_statement, conn)
+city_dict = {}
+for r in cityrows:
+    city_dict[r[1]] = r[0]
+
+# Creating dictionary mapping country to country ID
+sql_statement = """ SELECT * FROM Country; """
+countryrows = execute_sql_statement(sql_statement, conn)
+country_dict = {}
+for r in countryrows:
+    country_dict[r[1]] = r[0]
+
+# Creating GlobalLandTemperatureByMajorCity Table
+query = '''CREATE TABLE [GlobalLandTemperatureByMajorCity](
+                [ID] INTEGER NOT NULL PRIMARY KEY,
                 [Date] VARCHAR NOT NULL,
-                [City] TEXT NOT NULL,
-                [Country] INTEGER NOT NULL,
+                [CityID] INTEGER NOT NULL,
+                [CountryID] INTEGER NOT NULL,
                 [AverageTemperature] REAL,
-                [AverageTemperatureUncertainity] REAL)
+                [AverageTemperatureUncertainity] REAL,
+                FOREIGN KEY(CityID) REFERENCES MajorCities(CityID)
+                FOREIGN KEY(CountryID) REFERENCES Country(CountryID)
+                )
             '''
 create_table(conn, query)
 majorcityrecords = []
-for i in range(len(MajorCities)):
-    if int(MajorCities[i][0][0:4]) < 1850:
+
+for i in range(len(Cities)):
+    if int(Cities[i][0][0:4]) < 1900:
         continue
-    if MajorCities[i][1] == '' or MajorCities[i][2] == '':
+    if Cities[i][1] == '' or Cities[i][2] == '':
         continue
-    majorcityrecords.append((datetime.strptime(MajorCities[i][0], '%Y-%m-%d').strftime('%Y-%m-%d'), MajorCities[i][3], MajorCities[i][4], round(float(MajorCities[i][1]), 2), round(float(MajorCities[i][2]),2)))
+    majorcityrecords.append((datetime.strptime(Cities[i][0], '%Y-%m-%d').strftime('%Y-%m-%d'), city_dict.get(Cities[i][3]), country_dict.get(Cities[i][4]), round(float(Cities[i][1]), 2), round(float(Cities[i][2]),2)))
 
 with conn:
-    cur.executemany('''INSERT INTO GlobalLandTemperatureByMajorCity(Date, City, Country, AverageTemperature, AverageTemperatureUncertainity)
+    cur.executemany('''INSERT INTO GlobalLandTemperatureByMajorCity(Date, CityID, CountryID, AverageTemperature, AverageTemperatureUncertainity)
                         VALUES (?,?,?,?,?)''', majorcityrecords)
 
 header = None
@@ -95,7 +160,7 @@ with open('GlobalLandTemperaturesByCountry.csv', 'r') as file:
         Countries.append(line.strip().split(','))
 
 query = '''CREATE TABLE [GlobalLandTemperatureByCountry](
-                [CountryID] INTEGER NOT NULL PRIMARY KEY,
+                [ID] INTEGER NOT NULL PRIMARY KEY,
                 [Date] VARCHAR NOT NULL,
                 [Country] INTEGER NOT NULL,
                 [AverageTemperature] REAL,
@@ -104,7 +169,7 @@ query = '''CREATE TABLE [GlobalLandTemperatureByCountry](
 create_table(conn, query)
 countryrecords = []
 for i in range(len(Countries)):
-    if int(Countries[i][0][0:4]) < 1850:
+    if int(Countries[i][0][0:4]) < 1900:
         continue
     if Countries[i][1] == '' or Countries[i][2] == '':
         continue
@@ -134,7 +199,7 @@ query = '''CREATE TABLE [GlobalTemperatures](
 create_table(conn, query)
 globalrecords = []
 for i in range(len(GlobalTemperatures)):
-    if int(GlobalTemperatures[i][0][0:4]) < 1850:
+    if int(GlobalTemperatures[i][0][0:4]) < 1900:
         continue
     if GlobalTemperatures[i][1] == '' or GlobalTemperatures[i][2] == '':
         continue
@@ -144,9 +209,89 @@ with conn:
     cur.executemany('''INSERT INTO GlobalTemperatures(Date, AverageTemperature, AverageTemperatureUncertainity)
                         VALUES (?,?,?)''', globalrecords)
 
-'''Select CAST(strftime('%Y',Date)  AS INT) AS Year, Country, AverageTemperature, 
-    AverageTemperatureUncertainity from GlobalLandTemperatureByCountry GROUP BY Year, 
-    Country ORDER BY Country;'''
+# '''Select CAST(strftime('%Y',Date)  AS INT) AS Year, Country, AverageTemperature, 
+#     AverageTemperatureUncertainity from GlobalLandTemperatureByMajorCity GROUP BY Year, 
+#     City, Country ORDER BY City, Country;'''
+
+# df_majorcities = pd.read_sql_query('''Select CAST(strftime('%Y',Date)  AS INT) AS Year, City, Country, AverageTemperature, 
+#     AverageTemperatureUncertainity 
+# 	FROM GlobalLandTemperatureByMajorCity 
+# 	WHERE city in ('Bangalore', 'Bangkok', 'Paris', 'Harbin', 'Montreal', 'Moscow', 'Kiev', 'Toronto', 
+#     'Saint Petersburg', 'Tokyo', 'Berlin', 'Istanbul', 'Karachi', 'Dhaka', 'Rome', 'NewYork', 'Durban', 
+#     'Kano', 'Baghdad', 'Melbourne', 'Madrid', 'London', 'Berlin', 'Taiyuan', 'Florida')
+# 	GROUP BY Year, City, Country 
+# 	ORDER BY City, Country''', conn)
+
+Top25Cities = ['Bangalore', 'Bangkok', 'Paris', 'Harbin', 'Montreal', 'Moscow', 'Kiev', 'Toronto', 
+    'Saint Petersburg', 'Tokyo', 'Berlin', 'Istanbul', 'Karachi', 'Dhaka', 'Rome', 'NewYork', 'Durban', 
+    'Kano', 'Baghdad', 'Melbourne', 'Madrid', 'London', 'Berlin', 'Taiyuan', 'Florida']
+
+def checkStationarity(data):
+    data.index = data['Year']
+    pp.plot(data.index, data['AverageTemperature'])
+    #pp.legend(loc='best')
+    pp.title("Average Temperature of Major Cities from 1850 to 2013")
+    pp.show()
+
+    # Function to print out results in customised manner
+
+    from statsmodels.tsa.stattools import adfuller
+
+    Temps = data['AverageTemperature'].values
+    # split = len(Temps)//2
+    # Temps1, Temps2 = Temps[0:split], Temps[split:]
+    # meanTemps1, meanTemps2 = Temps1.mean(), Temps2.mean()
+    # varTemps1, varTemps2 = Temps1.var(), Temps2.var()
+
+    # if abs(meanTemps1-meanTemps2) <= 10 and abs(varTemps1-varTemps2) <= 10:
+    #     print('This indicates the given timeseries might be stationary as the mean and variance does not differ much.')
+    # else:
+    #     print('Given timeseries might not be stationary.')
+
+    #Performing Augmented Dickey-Fuller Test to confirm stationarity
+    AdfullerResult = adfuller(Temps)
+    print(AdfullerResult[1])
+    p_value = AdfullerResult[1]
+    if p_value < 0.05:
+        return 'Time series is stationary'
+    else:
+        return 'Time series is not stationary'
+
+# for c in Top25Cities:
+#     data = pd.read_sql_query('''Select CAST(strftime('%Y',Date)  AS INT) AS Year, City, Country, AverageTemperature, 
+#     AverageTemperatureUncertainity 
+# 	FROM GlobalLandTemperatureByMajorCity 
+# 	WHERE City = '''+str(c)+'''
+# 	GROUP BY Year, City, Country 
+# 	ORDER BY City, Country;''', conn)
+#     print(data)
+
+#df_majorcities.drop('ID', axis = 1)
+#print(df_majorcities)
+#print(df_majorcities.shape)
+# df_majorcities.index = df_majorcities['Year']
+# pp.plot(df_majorcities.index, df_majorcities['AverageTemperature'])
+# #pp.legend(loc='best')
+# pp.title("Average Temperature of Major Cities from 1850 to 2013")
+# pp.show()
+
+# # Function to print out results in customised manner
+
+# from statsmodels.tsa.stattools import adfuller
+
+# Temps = df_majorcities['AverageTemperature'].values
+# AdfullerResult = adfuller(Temps)
+# print(AdfullerResult[1])
+# p_value = AdfullerResult[1]
+# if p_value < 0.05:
+#     print('Time series is stationary')
+
+
+
+
+
+
+
 
 
 
